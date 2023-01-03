680f118929b7c7191cf9cd464ac645a7de0d1c92
Aleksandr <bestprogreraxe@gmail.com>
2023-01-03 05:12:40 +0300

    Solved Task64

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -5,3 +5,42 @@ N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"  */
 
 
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while (!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Error: wrong entry.");
+        }
+    }
+    return result;
+}
+
+void FinalCountdown(int number)
+{    
+    if (number >1)
+    {
+        Console.Write($"{number}, ");
+        FinalCountdown(number-1);
+    }
+    else
+    {
+        Console.Write($"{number}.");
+        Console.WriteLine();
+        return;
+    }
+}
+
+int number = GetNumber("Enter a number:");
+
+FinalCountdown(number);
